f13d1ccc2ecc201a51af2ac9e0a9a39e08ea74ec
Utku Utkan <utku.utkan@hoydaa.com>
2008-11-03 05:23:19 +0000

    Initial commit.

---
+++ apps/frontend/modules/user/lib/form/AccountSettingsForm.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class AccountSettingsForm extends sfForm
+{
+  protected static $genders = array(null, 'M' => 'Male', 'F' => 'Female');
+
+  public function configure()
+  {
+    $this->setWidgets(array(
+      'first_name' => new sfWidgetFormInput(),
+      'last_name' => new sfWidgetFormInput(),
+      'gender' => new sfWidgetFormSelect(array('choices' => self::$genders)),
+      'birthday' => new sfWidgetFormDate()
+    ));
+
+    $this->widgetSchema->setNameFormat('profile[%s]');
+
+    $this->setValidators(array(
+      'first_name' => new sfValidatorString(array('required' => true)),
+      'last_name' => new sfValidatorString(array('required' => true)),
+      'gender' => new sfValidatorChoice(array('choices' => array_keys(self::$genders))),
+      'birthday' => new sfValidatorDate(array('required' => false))
+    ));
+  }
+}
