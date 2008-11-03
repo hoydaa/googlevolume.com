bdb7f7ab6e2baf210dc6087d4e4351ea89eb2bf5
Utku Utkan <utku.utkan@hoydaa.com>
2008-11-03 05:22:03 +0000

    Initial commit.

---
+++ apps/frontend/modules/site/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * site actions.
+ *
+ * @package    project-y
+ * @subpackage site
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class siteActions extends sfActions
+{
+  public function executeMessage()
+  {
+  }
+}
