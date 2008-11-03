a7a2bc59692e1cb6818e6146f25f7dfff14e9191
Utku Utkan <utku.utkan@hoydaa.com>
2008-11-03 05:27:42 +0000

    Initial commit.

---
+++ lib/model/sfGuardUserProfile.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'sf_guard_user_profile' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class sfGuardUserProfile extends BasesfGuardUserProfile
+{
+  public function save($conn = null)
+  {
+    if($this->isNew())
+    {
+      $this->setActivationKey(md5(rand(100000, 999999) . $this->getEmail()));
+    }
+
+    parent::save($conn);
+  }
+}
