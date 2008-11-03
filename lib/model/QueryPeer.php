b25c8a1eea9089ca6e3b9f224c0da0813c9faf08
Utku Utkan <utku.utkan@hoydaa.com>
2008-11-03 05:27:11 +0000

    Initial commit.

--- lib/model/QueryPeer.php
+++ lib/model/QueryPeer.php
@@ -2,12 +2,30 @@
 
 class QueryPeer extends BaseQueryPeer
 {
-
   public static function retrieveByQUERY($query)
   {
     $c = new Criteria();
     $c->add(QueryPeer::QUERY, $query);
     return QueryPeer::doSelectOne($c);
+  }
+
+  public static function findUnprocessedQueries($date)
+  {
+    $c = new Criteria();
+    $c->add(QueryResultPeer::FETCH_DATE, $date);
+
+    $query_ids = array();
+    $query_results = QueryResultPeer::doSelect($c);
+
+    foreach ($query_results as $query_result)
+    {
+      $query_ids[] = $query_result->getQueryId();
+    }
+
+    $c = new Criteria();
+    $c->add(self::ID, $query_ids, Criteria::NOT_IN);
+
+    return QueryPeer::doSelect($c);
   }
 
 }
